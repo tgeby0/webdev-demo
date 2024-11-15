6fe11a0761539951161dc0c1bcaabcf3029febba
tgeby <tgeby@icloud.com>
2024-11-14 19:26:19 -0500

    Finished tutorial

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+document.getElementById("submitButton").addEventListener("click", addTask);
+function addTask(){
+    const taskInput = document.getElementById("taskInput").value;
+    //alert(`New Task: ${taskInput}`);
+    if (taskInput){
+        const newTask = document.createElement('li');
+        newTask.innerText = taskInput;
+        const taskList = document.getElementById('taskList');
+        taskList.appendChild(newTask);
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "removeButton";
+        deleteButton.innerText = "x";
+        deleteButton.addEventListener("click", () => taskList.removeChild(newTask));
+        newTask.appendChild(deleteButton);
+        document.getElementById("taskInput").value = '';
+    }
+}
